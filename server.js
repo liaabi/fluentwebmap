b3b7986e33b31c707c3f433dcb5560dcd4712b32
Ligia Arghir <ligia.arghir@gmail.com>
2014-06-02 11:49:14 +0200

    for debugging

--- server.js
+++ server.js
@@ -139,8 +139,10 @@ var App = function(){
     self.dbClient.connect(self.URI, function(err, database){
       if(err){ throw err };
       self.db = database;
+      echo "Database: ", database;
       self.db.authenticate(self.dbUser, self.dbPass, function(err, res){
         if(err){ throw err };
+        echo "authenticated";
         callback();
       });
     });
