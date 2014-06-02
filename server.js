fcf3f47f613438940eba7836a55e397e74bac32e
Ligia Arghir <ligia.arghir@gmail.com>
2014-06-02 14:10:22 +0200

    fix

--- server.js
+++ server.js
@@ -136,11 +136,11 @@ var App = function(){
   // Logic to open a database connection. We are going to call this outside of app so it is available to all our functions inside.
 
   self.connectDb = function(callback){
-    self.dbClient.connect(self.URI, function(err, db){
+    self.dbClient.connect(self.URI, function(err, database){
       if(err){ throw err };
       self.db = database;
       process.stdout.write("Database: " + database);
-      db.authenticate(self.dbUser, self.dbPass, function(err, res){
+      self.db.authenticate(self.dbUser, self.dbPass, function(err, res){
         if(err){ throw err };
         process.stdout.write("authenticated")
         callback();
